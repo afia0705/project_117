c8a753a4bdaf3fa2b7f998822323d8355afefff2
afia0705 <71982819+afia0705@users.noreply.github.com>
2021-02-17 02:53:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+    vidoe = createCapture(VIDEO)
+    vidoe.hide();
+    classifier = ml5.imageClassifier()
+}
+function draw() {
+    image(video , 0, 0, 300, 300);
+    classifier.classify(video , gotResult);
+}
+function modelLoaded() {
+    console.log('Model Loaded!')
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        
+    }
+}
